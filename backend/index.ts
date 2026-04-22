8a7cf35fcbfe616e250be1e353911334f1a4a74b
Vicky Yadav <ybicky86@gmail.com>
2026-04-23 01:48:53 +0530

    socket io implement done

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,19 @@
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
+import { createServer } from "http";
+import { initializeSocket } from "./src/utils/socket";
 
 const PORT = process.env.PORT || 3000;
 
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
         console.log(`Server is running on port http://localhost:${PORT}`);
     });
 }).catch((error) => {
     console.error(`Failed to connect to MongoDB: ${error}`);
 });
+
